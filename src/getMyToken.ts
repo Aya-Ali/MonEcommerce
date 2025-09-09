6d5e010ddb2dfdab426f0355e3a30cc07ebed12c
Aya-Ali <ayaali13477@gmail.com>
2025-09-09 10:31:06 +0300

    cookies

--- src/getMyToken.ts
+++ src/getMyToken.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export async function getMyToken() {
 
     const cookiesData = await cookies()
-    const DecodeToken = cookiesData.get("next-auth.session-token")?.value
+    const DecodeToken = cookiesData.get("__Secure-next-auth.session-token")?.value
 
     const data= await decode({ token: DecodeToken, secret: process.env.NEXTAUTH_SECRET! })
 
